24b22c4c9ec9f5ad448f8878a9f31c4de09fa6ad
Jens Blawatt <jens.blawatt@codecentric.de>
2023-09-26 23:17:30 +0200

    changes for light and motion

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,49 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define LIGHT_PIN 25
+#define MOTION_INPUT_PIN 26
+#define NUM_PIXELS 60
+#define LOOP_DELAY 500
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LIGHT_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  // put your setup code here, to run once:
-  // #Serial.begin(115200);
+  Serial.println("initializing...");
+  pinMode(MOTION_INPUT_PIN, INPUT);
+  Serial.begin(9600);
+  pixels.begin();
+  Serial.println("done");
+}
+
+void setLEDColor(uint32_t color, uint32_t delay_ms)
+{
+  for (int i = 0; i < NUM_PIXELS; i++)
+  {
+    pixels.setPixelColor(i, color);
+    pixels.show();
+    if (delay_ms > 0)
+    {
+      delay(delay_ms);
+    }
+  }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  Serial.println("hello world");
+  if (digitalRead(MOTION_INPUT_PIN) == HIGH)
+  {
+    Serial.println("motion");
+    setLEDColor(pixels.Color(255, 0, 0), 20); // RED
+  }
+  else
+  {
+    Serial.println("no motion");
+    setLEDColor(pixels.Color(0, 0, 0), 20); // OFF
+  }
+  delay(LOOP_DELAY);
 }
